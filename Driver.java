0f1cd21cc23b7e84607a3fd4835902dfd9397285
Muhammad Abdullah Qureshi <67544354+maq796113@users.noreply.github.com>
2023-08-09 10:46:02 +0500

    Commit 1

---
+++ Driver.java
@@ -0,0 +1,14 @@
+
+public class Driver {
+
+	public static void main(String[] args) {
+		if (args.length < 1) {
+			System.err.println("Usage: java ClassName <input_file>");
+			System.exit(0);
+		}
+		
+		String input_file = args[0];
+
+	}
+
+}

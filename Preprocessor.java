755376a05e98e3e8498dc1c82cedfa6c59d435ac
Muhammad Abdullah Qureshi <67544354+maq796113@users.noreply.github.com>
2023-08-10 21:20:45 +0500

    Commit 3

--- Preprocessor.java
+++ Preprocessor.java
@@ -3,15 +3,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+
 
 
 public class Preprocessor {
-	private String blank_lines;
-	private String tabs_spaces;
-	private String comments;
-	private String imports;
-	private String annotations;
+	
 	private String filePath;
 
 	public Preprocessor(String _filePath) {
@@ -28,7 +24,16 @@ public class Preprocessor {
 			try {
 				String line;
 				while ((line = reader.readLine()) != null) {
-					System.out.println(line);
+					if (line.trim().isEmpty() || line.contains("/*") || line.contains("//") || line.contains("import"))
+						continue;
+					line = line.trim().replaceAll("\\s+", " ");   //here '\\s+' is a regular expression which matches one or more whitespace characters (including spaces, tabs, and line breaks). besides \s is used to find whitespace
+					i = line.lastIndexOf(" ");   //get index of the last space
+					if (i >= 0 && i == line.length() - 2) //if lastIndex() function returns -1 then there is no space at all and check if the space is between the last two characters
+					{
+						line = line.substring(0, i) + line.substring(i+1);  //concatenate substrings going from first character to the character before the space between last two characters and the last character
+					}
+					
+					System.out.println(line); 
 				}
 				
 			} catch (IOException e) {

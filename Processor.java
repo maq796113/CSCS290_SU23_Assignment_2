389537128cc2e0651fdea57ed2a2596df6eb13db
Muhammad Abdullah Qureshi <67544354+maq796113@users.noreply.github.com>
2023-08-11 23:05:04 +0500

    Commit 4

--- Processor.java
+++ Processor.java
@@ -1,4 +1,35 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class Processor {
+public class Processor{
+	String _filePath;
+	public Processor(String filePath) {
+		this._filePath = filePath;
+		// TODO Auto-generated constructor stub
+	}
+
+	public void process() {
+		try {
+			boolean isnewline = false;  
+			FileReader fileReader = new FileReader(this._filePath);
+			FileWriter writer = new FileWriter("out2.txt");
+			int i;
+			while ((i = fileReader.read()) != -1) {    //read char by char
+				if ((char) i == '\n' || (char) i == '\r')   //check if there is a newline return character or carriage return char
+					isnewline = true;
+				else
+					writer.write(i);   //write char by char in the new file
+			}
+			writer.write((int) '$');
+			fileReader.close();
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("Failed Reading The File");
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
 
 }

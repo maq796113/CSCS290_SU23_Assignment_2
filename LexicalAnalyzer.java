140024c003dc97767e40b6311dce7fa622de301b
Muhammad Abdullah Qureshi <67544354+maq796113@users.noreply.github.com>
2023-08-15 02:35:05 +0500

    Update LexicalAnalyzer.java

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -13,43 +13,91 @@ public class LexicalAnalyzer {
 		try {
 			
 			FileReader fileReader = new FileReader(this._filePath);
+			FileReader fr = new FileReader(this._filePath);
 			int i;
-			int j = 0;
 			
-
-			char[] buffer = new char[100];
+			boolean flag = false;
+			boolean flag1 = false;
+			int j = 0;
+			int k;
+			int totalNumOfchars = 0;
+			int charsRead = 0;
+			
+			while ((k = fr.read()) != (int) '$') { //read the characters until you hit $ and ultimately count the number of chars
+				totalNumOfchars++;
+			}
+			fr.close();
 			System.out.print("Lexeme: ");
-			while ((i = fileReader.read()) != -1) {    //read char by char
-//				if ((char) i != ' ' && (char) i != '\0' && (char) i != '{' && (char) i != '(' && (char) i != '}' && (char) i != ')' && (char) i != ';') {
-//			
-//					buffer[j] = (char) i;
-//					j++;
-//				}
-//				
-//				else {
-//					j = 0;
-//					String lexeme = new String(buffer);
-//					lexeme = lexeme.replace("\0", "");
-//					System.out.println(lexeme);
-//				}
-				     //parenthesis: ()         curly brackets: {}    spaces   semi-colon
+			int countToCheckIfIncrement = 0;
+			while ((i = fileReader.read()) != (int) '$') {    //read char by char
+				charsRead++;
 				
-				if ((i > 39 && i < 42) || (i == 123 || i == 125) || i == 32 || i == 59) {     //trying to get all the separators
-					if (i != 32) {
-						System.out.println("\nLexeme: "+(char) i);
-						System.out.print("Lexeme: ");
+	
+				     //parenthesis: ()         curly brackets: {}    spaces   semi-colon :    comma ,     square brackets[]      period .
+				if ((i > 39 && i < 42) || (i == 123 || i == 125) || i == 32 || i == 59 || i ==  44   || i == 91 || i == 93    || i == 46) {     //trying to check for all punctuators
+					countToCheckIfIncrement = 0;
+					if (i != 32) {  // if it's not a space
+						
+						if (flag) {  //count to check if the previous character printed was a punctuator
+							j++;
+						}
+						
+						
+						
+						if ((flag && j>0) || flag1) {
+							System.out.println((char) i);   //print punctuators given that the previous charcter that was printed was also a punctuator
+						}
+						
+						
+						else {
+							System.out.println("\nLexeme: "+(char) i);   //print punctuators given that the previous charcter that was printed was not a punctuator
+						}
+						flag = true;
+						flag1 = false;
 					}
 						
-					else {
-						System.out.println();
-						System.out.print("Lexeme: ");
-					}
+					else {  
 						
+						if (!flag) {
+							System.out.println();
+						}
+						
+						flag1 = true;
+						flag = false;
+						j = 0;
+					}
 					
-					}
+					if ((flag1 || flag) && charsRead<totalNumOfchars)
+						System.out.print("Lexeme: ");
+					
+				}
 				
-				else
+				else { //print the character of a string lexeme char by char
+					
+					flag = false;
+					flag1 = false;
+					j = 0;
+					
+					if ((char) i == '+' || (char) i == '-') {
+						if (countToCheckIfIncrement == 1) {
+							System.out.print("\nLexeme: "+(char) i+(char) i);
+							continue;
+						}
+						
+						else {
+							countToCheckIfIncrement++;
+							continue;
+						}
+							
+						
+					}
+					
+					
+	
 					System.out.print((char) i);
+					
+				}
+				
 				
 			}
 			fileReader.close();
